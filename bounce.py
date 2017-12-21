bb1701e2fb5aa32ebf2ce41af535b593bd9a64e1
Alan Odom <clockmender@icloud.com>
2017-12-21 21:24:53 +0000

    First Revision

--- bounce.py
+++ bounce.py
@@ -9,9 +9,11 @@ class bounceNode(bpy.types.Node, AnimationNode):
     bl_label = "Bounce Node v 1.0"
     bl_width_default = 200
 
-    frm_s = FloatProperty(name = "Start Frame", default = 1, precision = 0)
-    frm_e = FloatProperty(name = "End Frame", default = 10, precision = 0, min = 10)
+    frm_s = IntProperty(name = "Start Frame", default = 1)
+    frm_e = IntProperty(name = "End Frame", default = 10, min = 10)
     speed = FloatProperty(name = "Cycle Speed", default = 4, min = 4)
+    spd_d = FloatProperty(name = "Speed Decay Factor", default = 1, precision = 3, min = 0.8, max = 1)
+    spd_v = FloatProperty(name = "Compute Factor", default = 1, precision = 2)
     hgt_s = FloatProperty(name = "Start Height", default = 1)
     hgt_b = FloatProperty(name = "Base Height",default = 0)
     message1 = StringProperty("")
@@ -24,6 +26,7 @@ class bounceNode(bpy.types.Node, AnimationNode):
         layout.prop(self, "frm_e")
         layout.prop(self, "speed")
         layout.prop(self, "spd_d")
+        layout.prop(self, "spd_v")
         layout.prop(self, "hgt_s")
         layout.prop(self, "hgt_b")
         if (self.message1 != ""):
@@ -36,10 +39,19 @@ class bounceNode(bpy.types.Node, AnimationNode):
             self.message1 = ""
             frm_c = bpy.context.scene.frame_current
             len_m = self.frm_e - self.frm_s
+            if frm_c == (self.frm_s - 1):
+                self.spd_v = self.speed
             if frm_c >= self.frm_s and frm_c <= self.frm_e:
+                if self.spd_d == 1:
+                    speed = self.speed
+                else:
+                    speed = self.spd_v
+                    if speed <= 4:
+                        speed = 4
                 fac_m = len_m - (frm_c - self.frm_s)
-                cos_w = abs(cos((frm_c - self.frm_s) * 2 * pi / (self.speed * 2)))
+                cos_w = abs(cos((frm_c - self.frm_s) * 2 * pi / (speed * 2)))
                 cos_w = (cos_w * fac_m * self.hgt_s / (self.frm_e - self.frm_s)) + self.hgt_b
+                self.spd_v = self.spd_v * (0.99 + (self.spd_d / 100))
             elif frm_c < self.frm_s:
                 cos_w = self.hgt_s + self.hgt_b
             else:
