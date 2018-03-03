33cda6eacf474af61d6e5933bfd9789515028ce6
Alan Odom <clockmender@icloud.com>
2018-03-03 13:26:49 +0000

    New Node + Updates 3 Mar 2018

--- speed_curves.py
+++ speed_curves.py
@@ -11,55 +11,56 @@ class createCurvesNode(bpy.types.Node, AnimationNode):
 
     message1 = StringProperty("")
     message2 = StringProperty("")
+    frm_j = IntProperty(name = "Jump Frame", default = 1, min = 1)
     exe_b = BoolProperty(name = "Execute", default = False, update = propertyChanged)
 
     def create(self):
-        self.newInput("Text", "Graph Name", "nam_c")
         self.newInput("Float", "Input", "val_i")
         self.newInput("Integer", "Start Frame", "frm_s")
         self.newInput("Integer", "End Frame", "frm_e")
         self.newInput("Float", "X Scale", "fac_x")
         self.newInput("Float", "Y Offset", "fac_y")
         self.newInput("Float", "Z Scale", "fac_z")
+        self.newInput("Object", "Object", "obj")
+        self.newOutput("Object", "Object", "obj")
 
     def draw(self,layout):
         layout.prop(self, "exe_b")
+        layout.prop(self, "frm_j")
         if (self.message2 != ""):
             layout.label(self.message2, icon = "INFO")
         if (self.message1 != ""):
             layout.label(self.message1, icon = "INFO")
 
-    def execute(self, nam_c, val_i, frm_s, frm_e, fac_x, fac_y, fac_z):
+    def execute(self, val_i, frm_s, frm_e, fac_x, fac_y, fac_z, obj):
+        if not obj:
+            # MAke sure mesh object is selected
+            self.message1 = "No Mesh Object"
+            return None
         if self.exe_b:
             self.message2 = "Processing"
             frm_p = bpy.context.scene.frame_start
-            if frm_e <= frm_s or frm_s <= frm_p + 1 or fac_x == 0 or fac_z == 0 or nam_c == "":
+            if frm_e <= frm_s or frm_s <= frm_p or fac_x == 0 or fac_z == 0:
                 self.message1 = "Set Frames/Scales, etc."
                 self.exe_b = False
             else:
                 frm_c = bpy.context.scene.frame_current
-                if frm_c == frm_s - 1:
-                    self.message1 = "Initial Processing"
-                    # Deselect All objects
-                    for obj in bpy.data.objects:
-                        obj.select = False
-                    # Make initial object
-                    mesh = bpy.data.meshes.new("mesh")  # add a new mesh
-                    obj = bpy.data.objects.new(nam_c, mesh)  # add a new object using the mesh
-                    scene = bpy.context.scene
-                    scene.objects.link(obj)  # put the object into the scene (link)
-                    scene.objects.active = obj  # set as the active object in the scene
-                    obj.select = True  # select object
-                    mesh = bpy.context.object.data
-                    bm = bmesh.new()
-                    # make the bmesh the object's mesh
-                    bm.to_mesh(mesh)
-                    bm.free()  # always do this when finished
-                elif frm_c >= frm_s and frm_c <= frm_e:
+                frm_i = frm_c - frm_s
+                if self.frm_j == 1:
+                    run_b = True
+                else:
+                    if frm_i % self.frm_j == 0:
+                        run_b = True
+                    else:
+                        run_b = False
+                if not run_b:
+                    self.message1 = "Skipping this frame"
+                elif run_b and frm_c >= frm_s and frm_c <= frm_e:
                     self.message1 = "Building Curves"
                     # Add curve points
+                    bpy.context.scene.objects.active = obj
+                    mesh = obj.data
                     vert = ((frm_c * fac_x), fac_y, (val_i * fac_z)) # next vert made with XYZ coords
-                    mesh = bpy.context.object.data
                     bm = bmesh.new()
                     # convert the current mesh to a bmesh (must be in edit mode)
                     bpy.ops.object.mode_set(mode='EDIT')
@@ -80,3 +81,5 @@ class createCurvesNode(bpy.types.Node, AnimationNode):
         else:
             self.message2 = "Not Processing"
             self.message1 = ""
+
+        return obj
